7a1f09368867b06a0eb4d68a403b76802611f611
Друщиц Сергей Николаевич <druschits_sn@peleng.jsc.local>
2024-01-15 13:12:14 +0300

    p1_ch2.py

---
+++ p1_ch2.py
@@ -0,0 +1,80 @@
+import numpy as np
+import datetime
+import torch
+import torch.optim as optim
+import torch.nn as nn
+import torch.functional as F
+from torch.utils.data import DataLoader, TensorDataset, random_split
+from torch.utils.tensorboard import SummaryWriter
+import matplotlib.pyplot as plt
+plt.style.use('fivethirtyeight')
+
+import os, sys
+sys.path.append(os.path.dirname(os.path.abspath(__file__)) + "\\stepbystep")
+from v0 import StepByStep
+
+######################################### DATA GENERATION ###########################################
+
+true_b = 1
+true_w = 2
+N = 100
+
+# Data Generation
+np.random.seed(42)
+x = np.random.rand(N, 1)
+y = true_b + true_w * x + (.1 * np.random.randn(N, 1))
+
+######################################### PREPARE DATA ##############################################
+torch.manual_seed(13)
+
+# Builds tensors from numpy arrays BEFORE split
+x_tensor = torch.as_tensor(x).float()
+y_tensor = torch.as_tensor(y).float()
+
+# Builds dataset containing ALL data points
+dataset = TensorDataset(x_tensor, y_tensor)
+
+# Performs the split
+ratio = .8
+n_total = len(dataset)
+n_train = int(n_total * ratio)
+n_val = n_total - n_train
+
+train_data, val_data = random_split(dataset, [n_train, n_val])
+
+# Builds a loader of each set
+train_loader = DataLoader(dataset=train_data, batch_size=16, shuffle=True)
+val_loader = DataLoader(dataset=val_data, batch_size=16)
+
+######################################## MODEL CONFIGURATION ########################################
+# Sets learning rate - this is "eta" ~ the "n" like Greek letter
+lr = 0.1
+
+torch.manual_seed(42)
+# Now we can create a model and send it at once to the device
+model = nn.Sequential(nn.Linear(1, 1))
+
+# Defines a SGD optimizer to update the parameters
+# (now retrieved directly from the model)
+optimizer = optim.SGD(model.parameters(), lr=lr)
+
+# Defines a MSE loss function
+loss_fn = nn.MSELoss(reduction='mean')
+
+print(model.state_dict())
+
+########################################################## MODEL TRAINING ###########################################
+
+sbs = StepByStep(model, loss_fn, optimizer)
+sbs.set_loaders(train_loader, val_loader)
+sbs.set_tensorboard('classy')
+
+print(sbs.model)
+
+sbs.train(n_epochs=200)
+
+print(model.state_dict()) # remember, model == sbs.model
+print(sbs.total_epochs)
+
+sbs.plot_losses()
+#plt.show()
