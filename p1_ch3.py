b1bbcf11d2d17a43ad29dc6b55e6754ebd3dc7f7
Друщиц Сергей Николаевич <druschits_sn@peleng.jsc.local>
2024-01-17 17:16:11 +0300

    p1_ch3.py

---
+++ p1_ch3.py
@@ -0,0 +1,88 @@
+import numpy as np
+import datetime
+import torch
+import torch.optim as optim
+import torch.nn as nn
+import torch.functional as F
+from torch.utils.data import DataLoader, TensorDataset, random_split
+from torch.utils.tensorboard import SummaryWriter
+import matplotlib.pyplot as plt
+plt.style.use('fivethirtyeight')
+
+from sklearn.datasets import make_moons
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix, roc_curve, precision_recall_curve, auc
+
+import os, sys
+sys.path.append(os.path.dirname(os.path.abspath(__file__)) + "\\stepbystep")
+from v0 import StepByStep
+
+######################################### DATA GENERATION ###########################################
+
+X, y = make_moons(n_samples=100, noise=0.3, random_state=0)
+
+X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=.2, random_state=13)
+
+sc = StandardScaler()
+sc.fit(X_train)
+
+X_train = sc.transform(X_train)
+X_val = sc.transform(X_val)
+
+######################################### PREPARE DATA ##############################################
+
+torch.manual_seed(13)
+
+# Builds tensors from numpy arrays
+x_train_tensor = torch.as_tensor(X_train).float()
+y_train_tensor = torch.as_tensor(y_train.reshape(-1, 1)).float()
+
+x_val_tensor = torch.as_tensor(X_val).float()
+y_val_tensor = torch.as_tensor(y_val.reshape(-1, 1)).float()
+
+# Builds dataset containing ALL data points
+train_dataset = TensorDataset(x_train_tensor, y_train_tensor)
+val_dataset = TensorDataset(x_val_tensor, y_val_tensor)
+
+# Builds a loader of each set
+train_loader = DataLoader(dataset=train_dataset, batch_size=16, shuffle=True)
+val_loader = DataLoader(dataset=val_dataset, batch_size=16)
+
+######################################## MODEL CONFIGURATION ########################################
+
+# Sets learning rate - this is "eta" ~ the "n" like Greek letter
+lr = 0.1
+
+torch.manual_seed(42)
+model = nn.Sequential()
+model.add_module('linear', nn.Linear(2, 1))
+
+print(model.state_dict())
+
+# Defines a SGD optimizer to update the parameters
+optimizer = optim.SGD(model.parameters(), lr=lr)
+
+# Defines a BCE loss function
+loss_fn = nn.BCEWithLogitsLoss()
+
+########################################################## MODEL TRAINING ###########################################
+
+n_epochs = 100
+
+sbs = StepByStep(model, loss_fn, optimizer)
+sbs.set_loaders(train_loader, val_loader)
+sbs.train(n_epochs)
+
+print(model.state_dict())
+
+########################################################### EVALUATE and VERIFY ######################################
+
+def sigmoid(z):
+    return 1 / (1 + np.exp(-z))
+
+logits_val = sbs.predict(X_val)
+probabilities_val = sigmoid(logits_val).squeeze()
+cm_thresh50 = confusion_matrix(y_val, (probabilities_val >= 0.5))
+print(cm_thresh50)
+
